310c8fadfe7b04b10ac7685cfbee731f7668a4f5
Federico Poggi <federicopoggi3@gmail.com>
2023-11-04 20:03:20 +0100

    ricevute coordinate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 import NavbarMain from "./components/NavbarMain.jsx";
+import {useState} from "react";
+
+
 function App() {
+    const [show, setResult] = useState(false)
 
+    return (
+        <div>
+            <NavbarMain/>
 
-  return (
-
-        <NavbarMain/>
-
-  )
+        </div>
+    )
 }
 
 export default App
--- src/components/NavbarMain.jsx
+++ src/components/NavbarMain.jsx
@@ -2,50 +2,64 @@ import {Component} from "react";
 import {Button, Col, Container, FormControl, InputGroup, Navbar} from "react-bootstrap";
 import {IonIcon} from "@ionic/react";
 import {search} from 'ionicons/icons'
+import {render} from "react-dom";
 
 
 class NavbarMain extends Component {
+
     state = {
         city: " ",
+        show: false,
+        lat: "",
+        lon: "",
     };
-    getCoordinate = async () => {
 
-        try {
-            const latLon = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.state.city}&limit=5&appid=5d5db0399a52dc58601818a37fda598b`)
-            if (latLon.ok) {
-                const coordinate = latLon.json()
-                console.log(coordinate);
-                return coordinate
-            }
-        } catch (err) {
-            throw new Error("Error getting data");
-        }
-
-
-    }
-
-
-    getWeather = async () => {
-        try {
-            const response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=5d5db0399a52dc58601818a37fda598b")
-            if (response.ok) {
-                console.log(response.json());
-
-            }
-        } catch (err) {
-            throw new Error("Error getting data");
-        }
-    }
 
     handleInputChange = (name, value) => {
         this.setState({
             ...this.state,
             [name]: value,
+            show: true,
         });
+    }
+
+
+    getCoordinate = async () => {
+
+        try {
+            const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.state.city}&limit=${1}&appid=5d5db0399a52dc58601818a37fda598b`)
+            if (response.ok) {
+                const coordinate = await response.json();
+                console.log(coordinate);
+                coordinate.map((lat) => {
+                    this.setState({
+                        ...this.state,
+                        lat: lat.lat,
+                        lon: lat.lon,
+                    })
+                    
+                })
+            }
+        } catch (err) {
+            throw new Error("Error getting data");
+        }
+    }
+
+    getMeteoCity = async () => {
+        try {
+            const meteo = await fetch(`api.openweathermap.org/data/2.5/forecast?lat=${this.state.lat}&lon=${this.state.lon}&appid=5d5db0399a52dc58601818a37fda598b`)
+            if (meteo.ok) {
+                const meteoData = await meteo.json();
+                console.log(meteoData);
+            }
+        } catch (err) {
+            throw new Error("err")
+        }
     }
 
     render() {
         return (
+
             <Container fluid={"sm"}>
                 <Navbar className={"d-flex justify-content-between"}>
                     <Col xl={6} lg={4}>
@@ -55,18 +69,22 @@ class NavbarMain extends Component {
                         <InputGroup className={"d-none d-sm-flex"}>
                             <FormControl placeholder={"Search"} onChange={(e) => {
                                 this.handleInputChange('city', e.target.value)
+
                             }}/>
-                            <Button variant={"outline-dark"} className={" d-flex flex-column justify-content-center"}
+                            <Button variant={"outline-dark"}
+                                    type={"submit"}
+                                    className={" d-flex flex-column justify-content-center"}
                                     onClick={(e) => {
                                         e.preventDefault();
-                                        this.getCoordinate()
+                                        this.getCoordinate();
                                     }}
-
                             ><IonIcon icon={search}></IonIcon></Button>
                         </InputGroup>
                     </Col>
                 </Navbar>
+
             </Container>
+            // da aggiungere sezione con risultati ricerc
         )
     }
 }
